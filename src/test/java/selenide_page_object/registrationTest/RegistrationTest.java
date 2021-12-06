eebd30a88b2c7ffc9aaac327343bf8354e86e320
valeriya <vperska@gmail.com>
2021-12-06 23:12:40 +0200

    different browsers for selenide test

--- src/test/java/selenide_page_object/registrationTest/RegistrationTest.java
+++ src/test/java/selenide_page_object/registrationTest/RegistrationTest.java
@@ -1,6 +1,11 @@
 package selenide_page_object.registrationTest;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
+import io.github.bonigarcia.wdm.managers.EdgeDriverManager;
+import io.github.bonigarcia.wdm.managers.FirefoxDriverManager;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import selenide_page_object.pages.MainPage;
@@ -9,6 +14,29 @@ import static com.codeborne.selenide.Selenide.*;
 
 
 public class RegistrationTest {
+
+    @Before
+    public void initialDriver() throws Exception {
+        initDriver();
+    }
+
+    public void initDriver() throws Exception {
+        String browser = System.getProperty("browser");
+        if ((browser == null) ||
+                ("chrome".equalsIgnoreCase(browser))) {
+            ChromeDriverManager.getInstance().setup();
+            Configuration.browser = "chrome";
+        } else if ("edge".equalsIgnoreCase(browser)) {
+            EdgeDriverManager.getInstance().setup();
+            Configuration.browser = "edge";
+        } else if ("firefox".equalsIgnoreCase(browser)) {
+            FirefoxDriverManager.getInstance().setup();
+            Configuration.browser = "firefox";
+        } else {
+            throw new Exception("Check browser var");
+        }
+    }
+
     @Test
     public void successRegistration() {
         open("https://finik-market.com/");
