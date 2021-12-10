c192245af42270f7f0502010d0f0ebfad201891e
valeriya <vperska@gmail.com>
2021-12-10 21:23:27 +0200

    some fix

--- src/main/java/selenide_page_object/pages/MainPage.java
+++ src/main/java/selenide_page_object/pages/MainPage.java
@@ -5,12 +5,12 @@ import com.codeborne.selenide.SelenideElement;
 import static com.codeborne.selenide.Selenide.*;
 
 public class MainPage {
-    final SelenideElement registrationIcon = $x(".//div[@class='userbar userbar--user-outline-icon j-user-tabs']//a[@href='#j-popup-tab-auth']");
-    final SelenideElement registrationTab = $x(".//a[@href='#j-popup-tab-signup']");
-    final SelenideElement nameInput = $x(".//input[@name='user[title]']");
-    final SelenideElement emailInput = $x(".//input[@name='user[email]' and @tabindex=2]");
-    final SelenideElement passInput = $x(".//input[@name='user[pass]' and @tabindex=3]");
-    final SelenideElement registerButton = $x(".//input[@tabindex=7 and @type='submit']");
+    private final SelenideElement registrationIcon = $x(".//div[@class='userbar userbar--user-outline-icon j-user-tabs']//a[@href='#j-popup-tab-auth']");
+    private final SelenideElement registrationTab = $x(".//a[@href='#j-popup-tab-signup']");
+    private final SelenideElement nameInput = $x(".//input[@name='user[title]']");
+    private final SelenideElement emailInput = $x(".//input[@name='user[email]' and @tabindex=2]");
+    private final SelenideElement passInput = $x(".//input[@name='user[pass]' and @tabindex=3]");
+    private final SelenideElement registerButton = $x(".//input[@tabindex=7 and @type='submit']");
 
 
     public void clickToRegistrationIcon() {
--- src/test/java/selenide_page_object/registrationTest/local/LocalFirefoxDriver.java
+++ src/test/java/selenide_page_object/registrationTest/local/LocalFirefoxDriver.java
@@ -9,12 +9,15 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 
 import javax.annotation.Nonnull;
 
-public abstract class LocalFirefoxDriver implements WebDriverProvider {
+public abstract class LocalFirefoxDriver implements WebDriverProvider { //static
     @Nonnull
+//    @Override
     public WebDriver createDriver(@Nonnull DesiredCapabilities desiredCapabilities) {
         WebDriverManager.firefoxdriver().setup();
         FirefoxOptions firefoxOptions = new FirefoxOptions();
         firefoxOptions.setHeadless(true);
+        firefoxOptions.addArguments("--proxy-bypass-list=<-loopback>");
+        firefoxOptions.merge(desiredCapabilities);
         return new FirefoxDriver(firefoxOptions);
     }
 }
